50fd6fec38a2f5a1def381959d1ba5ce6187fb5a
eerickwrld <160683152+eerickwrld@users.noreply.github.com>
2024-02-20 22:25:25 -0300

    Create atividade3.java

---
+++ atividade3.java
@@ -0,0 +1,9 @@
+public class Identificacao{
+    public static void(string[] args){
+        string nome = "Angelo";
+        int idade = '5';
+        float nota1 = '6.6';
+        float nota2 = '5.5';
+        double media = '';
+    }
+}
