61262372108e1217ceb9a8fbeb15c4d829ff51f5
eerickwrld <160683152+eerickwrld@users.noreply.github.com>
2024-02-20 22:18:48 -0300

    Create atividade1.java

---
+++ atividade1.java
@@ -0,0 +1,13 @@
+public class Varviaveis{
+    public static void(string[] args){
+    string nome = "Erick";
+    string profissao = "jogador";
+    int idade = 15;
+    char genero = 'M';
+    boolean estudante = true; 
+    float peso = 50.0;
+    double altura = 1.78;
+    short telefone = 46991;
+    long cpf = 12343254525;
+    }
+}
