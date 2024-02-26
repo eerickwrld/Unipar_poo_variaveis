95e57a7f49f2adaed80b654922ef3f157625556d
eerickwrld <160683152+eerickwrld@users.noreply.github.com>
2024-02-26 17:59:56 -0300

    Create atividade5.java

---
+++ atividade5.java
@@ -0,0 +1,23 @@
+public class main InformacoesProdutos{
+    public static void(string[] args){
+String Smartphone = "s24";
+String MarcaSmph = "Samsung";
+double PrecoSmph = "3500,00";
+int QtddSmph = "30";
+
+String Laptop = "nitro 5";
+String MarcaNote = "Acer";
+double PrecoNote = "5000,00";
+int QtddNote = "50";
+
+String FoneDeOuvido = "Air Dots 4";
+String MarcaFone = "Apple";
+double PrecoFone = "299,00";
+int QtddFone = "23";
+
+System.out.println("Informações sobre os produtos:");
+        System.out.println("Smartphone: " + Smartphone + ", Preço: R$" + PrecoSmph + ", Quantidade disponível: " + QtddSmph);
+        System.out.println("Laptop: " + Laptop + ", Preço: R$" + PrecoNote + ", Quantidade disponível: " + QtddNote);
+        System.out.println("Fone de ouvido: " + FoneDeOuvido + ", Preço: R$" + PrecoFone + ", Quantidade disponível: " + QtddFone);
+    }
+}
