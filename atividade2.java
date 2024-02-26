814aa2cabcef335a6248014fb0bbf2efcca2e3cd
eerickwrld <160683152+eerickwrld@users.noreply.github.com>
2024-02-26 17:20:21 -0300

    Create atividade2.java

---
+++ atividade2.java
@@ -0,0 +1,27 @@
+public class main NomeProduto1{
+    
+}
+public class main PrecoProduto%{
+    
+}
+public class main QuantidadeProduto{
+    
+}
+public class main DescricaoProduto{
+    
+}
+public class main NomeProduto{
+    
+}
+public class main NumeroProduto_123{
+    
+}
+public class main PrecoDoProduto{
+    
+}
+public class main EstoqueDisponivel{
+    
+}
+public class main TotalDeProdutos{
+    
+}
