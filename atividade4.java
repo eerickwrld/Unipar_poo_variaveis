4a1dc5639e0dfc097e5579809dadb6a880741e26
eerickwrld <160683152+eerickwrld@users.noreply.github.com>
2024-02-26 17:43:20 -0300

    Create atividade4.java

---
+++ atividade4.java
@@ -0,0 +1,8 @@
+public class main ControleEstoque{
+    public static void(string[] args){
+        String NomeProduto = "PRODUTO 1";
+        String CategoriaProduto = "1-Alimento, 2-Higiene, 3-Bebidas";
+        float PrecoProduto = '1...100';
+        int QuandidadeEstoque = '1...1000';
+    }
+}
